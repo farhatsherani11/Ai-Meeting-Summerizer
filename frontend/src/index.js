e2190e85605e9f8ad2964e678dcad96862f82ccc
farhat <farhatsherani08@gmail.com>
2025-07-26 16:10:03 +0530

    farhat

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ClerkProvider } from '@clerk/clerk-react';
+
+const clerkFrontendApi = 'pk_test_c3dlZXBpbmctbGFtcHJleS03OS5jbGVyay5hY2NvdW50cy5kZXYk';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={clerkFrontendApi}>
+      <App />
+    </ClerkProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Optional: Performance reporting
 reportWebVitals();
