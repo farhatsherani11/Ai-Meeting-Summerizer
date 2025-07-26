02f7d0935ee541bb6e5409baa3f8e1bb69a4d681
Shalvi Khare <shalvi.khare2007@gmail.com>
2025-07-26 18:55:34 +0530

    shalvi khare

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,45 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ClerkProvider } from '@clerk/clerk-react';
 
-
-import {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  RedirectToSignIn,
-  useClerk,
-} from '@clerk/clerk-react';
-import { BrowserRouter } from 'react-router-dom';
-
-const clerkFrontendApi = 'pk_test_c3dlZXBpbmctbGFtcHJleS03OS5jbGVyay5hY2NvdW50cy5kZXYk'; // replace with actual
-
-// Custom component to sign out users on load
-function SignOutOnLoad({ children }) {
-  const { signOut } = useClerk();
-
-  React.useEffect(() => {
-    signOut();
-  }, []);
-
-  return null;
-}
+const clerkFrontendApi = 'pk_test_c3dlZXBpbmctbGFtcHJleS03OS5jbGVyay5hY2NvdW50cy5kZXYk'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ClerkProvider publishableKey={clerkFrontendApi}>
-      <BrowserRouter>
-        <SignOutOnLoad />
-        <SignedIn>
-          <App />
-        </SignedIn>
-        <SignedOut>
-          <RedirectToSignIn />
-        </SignedOut>
-      </BrowserRouter>
+      <App />
     </ClerkProvider>
   </React.StrictMode>
 );
 
+// Optional: Performance reporting
 reportWebVitals();
