9485794c05fd9a1dc37dd4a66eb226f09562155d
Shalvi Khare <shalvi.khare2007@gmail.com>
2025-07-26 17:21:50 +0530

    shalvi frontend

--- frontend/src/(auth)/sign-in.js
+++ frontend/src/(auth)/sign-in.js
@@ -1,5 +1,4 @@
-  import { SignIn } from '@clerk/nextjs'
-
+  import { SignIn } from '@clerk/clerk-react';
 export default function Page() {
   return (
         <section className="bg-black dark:bg-gray-900">
@@ -7,7 +6,7 @@ export default function Page() {
             <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
             <img
                 alt=""
-                src="https://t3.ftcdn.net/jpg/02/76/26/88/360_F_276268861_Y4ZHLZuZoXVkrXsjaHkaJn7xbE57dz81.jpg"
+                src= "image"
                 className="absolute inset-0 h-full w-full object-cover opacity-80"
             />
 
